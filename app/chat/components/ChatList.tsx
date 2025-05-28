4bb9612dd793d20d0a80d3b6ae6e1c3126340855
Davis Inde Satya <davis.onde.wei@gmail.com>
2025-05-28 09:10:02 +0700

    refactor function

--- app/chat/components/ChatList.tsx
+++ app/chat/components/ChatList.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from "react"
+import { useEffect, useMemo, useRef } from "react"
 import { ChatListState, ChatRoomUiState } from "../_model/ChatRoomUiState"
 import { ChatBubble, ModeratorBubble } from "./ChatBubble"
 
@@ -17,9 +17,9 @@ type ChatListProps = {
 
 export const ChatListScreen = ({state, listState}: { state: ChatState, listState: ChatRoomUiState }) => {
     const animation = useMemo(() => state.type === "open" ? "show-chat" : "show-chat", [state])
-    return <div className={`absolute top-1/4 left-1/4 w-1/2 h-1/2 transition-all show-chat ${animation}`}>
+    return <div className={`absolute left-1/4 w-1/2 h-screen max-h-screen transition-all show-chat ${animation}`}>
         {state.type === "open" &&
-            <ChatList
+            <ChatListContent
                 modMessage={state.modMessage}
                 chatState={listState}
             />
@@ -27,25 +27,44 @@ export const ChatListScreen = ({state, listState}: { state: ChatState, listState
     </div>
 }
 
-export const ChatList = ({...props}: ChatListProps) => {
+const ChatListContent = ({...props}: ChatListProps) => {
     switch(props.chatState.type) {
         case "loaded": {
-            const chatBubbles = props.chatState.data.chatList.map(chat => 
-                <ChatBubble 
-                    key={chat.turnId} 
-                    message={chat}
-                />
-            )
-            return <>
-                <div className="w-full h-full flex flex-col rounded-xl border border-white p-8 bg-slate-800">
-                    <ModeratorBubble message={props.modMessage}/>
-                    <ul className="flex flex-col max-h-1/3 h-1/3 gap-3 w-full flex-grow mt-5 overflow-y-scroll">
-                        {chatBubbles}
-                    </ul>
-                </div>
-            </>  
+            return <ChatList 
+                modMessage={props.modMessage}
+                state={props.chatState.data}
+            />
         }
         case "error": {}
         default:
     }
+}
+
+const ChatList = ({...props}: {modMessage: string, state: ChatListState}) => {
+    const chatBubbles = props.state.chatList.map(chat => 
+        <ChatBubble 
+            key={chat.turnId} 
+            message={chat}
+        />
+    )
+
+    const listRef = useRef<HTMLUListElement>(null!)
+
+    useEffect(() => {
+        const ref = listRef.current
+        if (!ref) return 
+
+        ref.scrollIntoView()
+    }, [chatBubbles])
+    return <>
+        <div className="z-10 w-full h-full flex flex-col rounded-xl border border-white p-8 bg-slate-800">
+            <ModeratorBubble message={props.modMessage}/>
+            <ul 
+                className="flex flex-col max-h-1/3 h-1/3 gap-3 w-full flex-grow mt-5 overflow-y-auto"
+                ref={listRef}
+            >
+                {chatBubbles}
+            </ul>
+        </div>
+    </> 
 }
